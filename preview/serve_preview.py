2ce367d777044a004b9d3fe6870b5957fd53b339
racer-leo9 <leo033305@gmail.com>
2026-06-10 14:23:24 +0800

    add serve_preview.py

---
+++ preview/serve_preview.py
@@ -0,0 +1,13 @@
+﻿from waitress import serve
+from flask import Flask, send_file
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file(os.path.join(os.path.dirname(__file__), '..', 'preview', 'competitor.html'))
+
+if __name__ == '__main__':
+    print('Preview server running on http://localhost:5002')
+    serve(app, host='0.0.0.0', port=5002)
