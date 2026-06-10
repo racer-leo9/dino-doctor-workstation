b3717711dee7593662614b4f1a45bd946661c8a9
racer-leo9 <leo033305@gmail.com>
2026-06-10 14:23:04 +0800

    add num_helper.py

---
+++ skills/video-analyzer/scripts/num_helper.py
@@ -0,0 +1,9 @@
+﻿import re
+
+def safe_int(val, default=0):
+    if isinstance(val, (int, float)):
+        return int(val)
+    if isinstance(val, str):
+        m = re.search(r'\d+', val)
+        return int(m.group()) if m else default
+    return default
